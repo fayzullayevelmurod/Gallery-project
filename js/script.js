37c5bba7bbe264075da8f8acc9c4b5d643418ee3
SardorQobilov <sardorqobilov555@gmail.com>
2024-12-16 19:49:18 +0500

    S

--- js/script.js
+++ js/script.js
@@ -6,6 +6,70 @@ const artSwiper = new Swiper('.art-slider', {
     nextEl: '.art-slider-next',
     prevEl: '.art-slider-prev',
   },
+  speed: 700,
+  // breakpoints: {
+  //   768: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //   },
+  //   1024: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30,
+  //   },
+  // },
+});
+
+// art-slider-4
+const artSwiper4 = new Swiper('.art-slider-4', {
+  slidesPerView: 4,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.art-slider-next',
+    prevEl: '.art-slider-prev',
+  },
+  speed: 700,
+  // breakpoints: {
+  //   768: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //   },
+  //   1024: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30,
+  //   },
+  // },
+});
+
+// big-art__swiper
+const artSwiper2 = new Swiper('.art-swiper__2', {
+  slidesPerView: 1.8,
+  spaceBetween: 40,
+  speed: 700,
+  navigation: {
+    nextEl: '.art-slider-next-2',
+    prevEl: '.art-slider-prev-2',
+  },
+  // breakpoints: {
+  //   768: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //   },
+  //   1024: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30,
+  //   },
+  // },
+});
+
+// see-art__swiper
+const seeArtSwiper = new Swiper('.see-art__swiper', {
+  slidesPerView: 3.5,
+  spaceBetween: 40,
+  speed: 700,
+  navigation: {
+    nextEl: '.art-slider-next-2',
+    prevEl: '.art-slider-prev-2',
+  },
   // breakpoints: {
   //   768: {
   //     slidesPerView: 2,
