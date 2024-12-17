d157a10edfd48c4f06bf329b21517da94e10f799
SardorQobilov <sardorqobilov555@gmail.com>
2024-12-17 12:18:59 +0500

    S

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 // art-swiper
-const artSwiper = new Swiper('.art-slider', {
+const artSwiper = new Swiper('.art-slider-1', {
   slidesPerView: 3.5,
   spaceBetween: 40,
   navigation: {
@@ -22,13 +22,57 @@ const artSwiper = new Swiper('.art-slider', {
   watchOverflow: false,
 });
 
-// art-slider-4
-const artSwiper4 = new Swiper('.art-slider-4', {
-  slidesPerView: 4,
+// art-slider-1.1
+const artSwiper11 = new Swiper('.art-slider-1-1', {
+  slidesPerView: 3.5,
   spaceBetween: 40,
   navigation: {
-    nextEl: '.art-slider-next',
-    prevEl: '.art-slider-prev',
+    nextEl: '.art-slider-next-1-1',
+    prevEl: '.art-slider-prev-1-1',
+  },
+  speed: 700,
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
+    },
+    700: {
+      slidesPerView: 2.2,
+    },
+    900: {
+      slidesPerView: 3.5,
+    },
+  },
+  watchOverflow: false,
+});
+// art-slider-2
+const artSwiper22 = new Swiper('.art-slider-2', {
+  slidesPerView: 3,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.art-slider-next-2-2',
+    prevEl: '.art-slider-prev-2-2',
+  },
+  speed: 700,
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
+    },
+    700: {
+      slidesPerView: 2.2,
+    },
+    900: {
+      slidesPerView: 3.5,
+    },
+  },
+  // watchOverflow: false,
+});
+// art-slider-4
+const artSwiper4 = new Swiper('.art-slider-4', {
+  slidesPerView: 5,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.art-slider-next-4',
+    prevEl: '.art-slider-prev-4',
   },
   speed: 700,
   breakpoints: {
@@ -43,7 +87,174 @@ const artSwiper4 = new Swiper('.art-slider-4', {
     },
   },
 });
-
+// art-slider-5
+const artSwiper5 = new Swiper('.art-slider-5', {
+  slidesPerView: 3.5,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.art-slider-next-5',
+    prevEl: '.art-slider-prev-5',
+  },
+  speed: 700,
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
+    },
+    700: {
+      slidesPerView: 2.2,
+    },
+    900: {
+      slidesPerView: 3.5,
+    },
+  },
+});
+// art-slider-5
+const artSwiper6 = new Swiper('.art-slider-6', {
+  slidesPerView: 3.5,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.art-slider-next-6',
+    prevEl: '.art-slider-prev-6',
+  },
+  speed: 700,
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
+    },
+    700: {
+      slidesPerView: 2.2,
+    },
+    900: {
+      slidesPerView: 3.5,
+    },
+  },
+});
+// art-slider-7
+const artSwiper7 = new Swiper('.art-slider-7', {
+  slidesPerView: 3.5,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.art-slider-next-7',
+    prevEl: '.art-slider-prev-7',
+  },
+  speed: 700,
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
+    },
+    700: {
+      slidesPerView: 2.2,
+    },
+    900: {
+      slidesPerView: 3.5,
+    },
+  },
+});
+// art-slider-8
+const artSwiper8 = new Swiper('.art-slider-8', {
+  slidesPerView: 3.5,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.art-slider-next-8',
+    prevEl: '.art-slider-prev-8',
+  },
+  speed: 700,
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
+    },
+    700: {
+      slidesPerView: 2.2,
+    },
+    900: {
+      slidesPerView: 3.5,
+    },
+  },
+});
+// art-slider-9
+const artSwiper9 = new Swiper('.art-slider-9', {
+  slidesPerView: 3.5,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.art-slider-next-9',
+    prevEl: '.art-slider-prev-9',
+  },
+  speed: 700,
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
+    },
+    700: {
+      slidesPerView: 2.2,
+    },
+    900: {
+      slidesPerView: 3.5,
+    },
+  },
+});
+// art-slider-9
+const artSwiper10 = new Swiper('.art-slider-10', {
+  slidesPerView: 3.5,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.art-slider-next-10',
+    prevEl: '.art-slider-prev-10',
+  },
+  speed: 700,
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
+    },
+    700: {
+      slidesPerView: 2.2,
+    },
+    900: {
+      slidesPerView: 3.5,
+    },
+  },
+});
+// art-slider-9
+const artSwiper12 = new Swiper('.art-slider-12', {
+  slidesPerView: 3.5,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.art-slider-next-12',
+    prevEl: '.art-slider-prev-12',
+  },
+  speed: 700,
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
+    },
+    700: {
+      slidesPerView: 2.2,
+    },
+    900: {
+      slidesPerView: 3.5,
+    },
+  },
+});
+// art-slider-9
+const artSwiper13 = new Swiper('.art-slider-13', {
+  slidesPerView: 3.5,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: '.art-slider-next-13',
+    prevEl: '.art-slider-prev-13',
+  },
+  speed: 700,
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
+    },
+    700: {
+      slidesPerView: 2.2,
+    },
+    900: {
+      slidesPerView: 3.5,
+    },
+  },
+});
 // big-art__swiper
 const artSwiper2 = new Swiper('.art-swiper__2', {
   slidesPerView: 1.8,
@@ -62,6 +273,7 @@ const artSwiper2 = new Swiper('.art-swiper__2', {
     },
   },
 });
+
 
 // see-art__swiper
 const seeArtSwiper = new Swiper('.see-art__swiper', {
@@ -119,8 +331,8 @@ let gallerySwiper = new Swiper('.gallery-slider-2', {
   spaceBetween: 40,
   speed: 700,
   navigation: {
-    nextEl: '.art-work-next',
-    prevEl: '.art-work-prev',
+    nextEl: '.gallery-slider-2-next',
+    prevEl: '.gallery-slider-2-prev',
   },
   breakpoints: {
     0: {
@@ -141,3 +353,26 @@ window.addEventListener('load', () => {
     button.classList.remove('swiper-button-lock');
   });
 });
+
+
+try {
+  // Cookie banner elementlarini tanlash
+  const cookieBanner = document.querySelector('.cookie');
+  const closeCookieButton = document.querySelector('.close-cookie');
+
+  // Cookie holatini tekshirish
+  if (!localStorage.getItem('cookieAccepted')) {
+    // Cookie bannerini ko‘rsatish
+    cookieBanner.style.display = 'flex';
+  }
+
+  // "Accept" tugmasi bosilganda
+  closeCookieButton.addEventListener('click', () => {
+    // Cookie qabul qilindi holatini saqlash
+    localStorage.setItem('cookieAccepted', 'true');
+    // Cookie bannerini yashirish
+    cookieBanner.style.display = 'none';
+  });
+} catch (error) {
+
+}
